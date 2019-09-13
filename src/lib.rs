eb92acb89a6e9a42196e8f26c6978fccc2d7d3cb
Aurora Execution <auroraexecution142857@gmail.com>
2019-09-13 11:23:01 +0800

    Switch between every pages.
    Signed-off-by: Aurora Execution <auroraexecution142857@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -4,30 +4,65 @@ use seed::prelude::*;
 
 
 // Model
-
 struct Model {
-    pub val: i32,
+    page: Page
 }
 
 impl Default for Model {
     fn default() -> Self {
         Self {
-            val: 0,
+            page: Page::EventList
         }
     }
 }
 
+#[derive(Clone)]
+enum Page {
+    EventList,    // as index now
+    AccountState,
+    VerifyRequest,
+    GenerateAuthorization,
+//    PageNotFound
+}
 
-// Update
 
 #[derive(Clone)]
 enum Msg {
-    Increment,
+    PageFowardTo(Page),
+}
+
+#[wasm_bindgen(start)]
+pub fn render() {
+    seed::App::build(|_, _| Model::default(), update, view)
+        .routes(routes)
+        .finish()
+        .run();
+}
+
+fn routes(url: seed::Url) -> Msg {
+    if url.path.is_empty() {
+        return Msg::PageFowardTo(Page::EventList);
+    }
+
+    match url.path[0].as_ref() {
+        "account_state" => {
+            Msg::PageFowardTo(Page::AccountState)
+        },
+        "verify_request" => {
+            Msg::PageFowardTo(Page::VerifyRequest)
+        },
+        "generate_authorization" => {
+            Msg::PageFowardTo(Page::GenerateAuthorization)
+        },
+        _ => Msg::PageFowardTo(Page::EventList)
+    }
 }
 
 fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
     match msg {
-        Msg::Increment => model.val += 1,
+        Msg::PageFowardTo(page) => {
+            model.page = page;
+        },
     }
 }
 
@@ -35,16 +70,40 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
 // View
 
 fn view(model: &Model) -> impl View<Msg> {
-    button![
-        simple_ev(Ev::Click, Msg::Increment),
-        format!("Hello, World × {}", model.val)
+    let page_content_node: Node<Msg> = match model.page {
+        Page::EventList => {
+            div!["This is event list page"]
+        },
+        Page::AccountState => {
+            div!["This is account state page"]
+        },
+        Page::VerifyRequest => {
+            div!["This is verify request page"]
+        },
+        Page::GenerateAuthorization => {
+            div!["This is generateauthorization page"]
+        }
+    };
+
+    div![
+        div![class!["navigation"],
+             div![class!["logo"],
+                  "Litentry Logo"
+             ],
+             div![class!["navigator"],
+                  div![class!["page_tab"],
+                       a!["Account State", attrs!{At::Href => "/account_state"}]
+                  ],
+                  div![class!["page_tab"],
+                       a!["Verify Request", attrs!{At::Href => "/verify_request"}]
+                  ],
+                  div![class!["page_tab"],
+                       a!["GenerateAuthorization", attrs!{At::Href => "/generate_authorization"}]
+                  ],
+             ],
+        ],
+        div![class!["page_content"],
+             page_content_node
+        ],
     ]
-}
-
-#[wasm_bindgen(start)]
-pub fn render() {
-    seed::App::build(|_, _| Model::default(), update, view)
-        .finish()
-        .run();
-
 }
