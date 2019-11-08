7a1d7d48b9e7f6a7c4e0ed0357d7d61707b4d5de
Aurora Execution <auroraexecution142857@gmail.com>
2019-11-08 13:17:27 +0800

    update to seed 0.4.2
    Signed-off-by: Aurora Execution <auroraexecution142857@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -21,12 +21,12 @@ pub struct Model {
 
 impl Default for Model {
     fn default() -> Self {
-	Self {
-	    page: Page::EventList,
-	    pas_model: pas::Model::default(),
-	    pvr_model: pvr::Model::default(),
-	    pga_model: pga::Model::default(),
-	}
+        Self {
+            page: Page::EventList,
+            pas_model: pas::Model::default(),
+            pvr_model: pvr::Model::default(),
+            pga_model: pga::Model::default(),
+        }
     }
 }
 
@@ -54,59 +54,59 @@ pub enum Msg {
 
 #[wasm_bindgen(start)]
 pub fn render() {
-    seed::App::build(|_, _| Model::default(), update, view)
-	.routes(routes)
-	// `trigger_update_handler` is necessary,
-	// because we want to process `seed::update(..)` calls.
-	//.window_events(|_| vec![trigger_update_handler()])
-	.finish()
-	.run();
+    seed::App::build(|_, _| Init::new(Model::default()), update, view)
+        .routes(routes)
+        // `trigger_update_handler` is necessary,
+        // because we want to process `seed::update(..)` calls.
+        //.window_events(|_| vec![trigger_update_handler()])
+        .finish()
+        .run();
 }
 
-fn routes(url: seed::Url) -> Msg {
+fn routes(url: seed::Url) -> Option<Msg> {
     if url.path.is_empty() {
-	return Msg::PageFowardTo(Page::EventList);
+        return Some(Msg::PageFowardTo(Page::EventList));
     }
 
     match url.path[0].as_ref() {
-	"account_state" => {
-	    Msg::PageFowardTo(Page::AccountState)
-	},
-	"verify_request" => {
-	    Msg::PageFowardTo(Page::VerifyRequest)
-	},
-	"generate_authorization" => {
-	    Msg::PageFowardTo(Page::GenerateAuthorization)
-	},
-	_ => Msg::PageFowardTo(Page::EventList)
+        "account_state" => {
+            Some(Msg::PageFowardTo(Page::AccountState))
+        },
+        "verify_request" => {
+            Some(Msg::PageFowardTo(Page::VerifyRequest))
+        },
+        "generate_authorization" => {
+            Some(Msg::PageFowardTo(Page::GenerateAuthorization))
+        },
+        _ => Some(Msg::PageFowardTo(Page::EventList))
     }
 }
 
 fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
     match msg {
-	Msg::PageFowardTo(page) => {
-	    // switch page
-	    model.page = page.clone();
-	    // do child page initialization
-	    match page {
-		Page::AccountState => pas::init(&mut model.pas_model, &mut orders.proxy(Msg::Pas)),
-		Page::VerifyRequest => pvr::init(&mut model.pvr_model, &mut orders.proxy(Msg::Pvr)),
-		Page::GenerateAuthorization => pga::init(&mut model.pga_model, &mut orders.proxy(Msg::Pga)),
-		Page::EventList => {}
-	    }
-	},
-	Msg::Pas(msg) => {
-	    pas::update(msg, &mut model.pas_model, &mut orders.proxy(Msg::Pas));
-	}
-	Msg::Pvr(msg) => {
-	    pvr::update(msg, &mut model.pvr_model, &mut orders.proxy(Msg::Pvr));
-	}
-	Msg::Pga(msg) => {
-	    pga::update(msg, &mut model.pga_model, &mut orders.proxy(Msg::Pga));
-	},
-	Msg::Test(astr) => {
-	    log!(astr)
-	}
+        Msg::PageFowardTo(page) => {
+            // switch page
+            model.page = page.clone();
+            // do child page initialization
+            match page {
+                Page::AccountState => pas::init(&mut model.pas_model, &mut orders.proxy(Msg::Pas)),
+                Page::VerifyRequest => pvr::init(&mut model.pvr_model, &mut orders.proxy(Msg::Pvr)),
+                Page::GenerateAuthorization => pga::init(&mut model.pga_model, &mut orders.proxy(Msg::Pga)),
+                Page::EventList => {}
+            }
+        },
+        Msg::Pas(msg) => {
+            pas::update(msg, &mut model.pas_model, &mut orders.proxy(Msg::Pas));
+        }
+        Msg::Pvr(msg) => {
+            pvr::update(msg, &mut model.pvr_model, &mut orders.proxy(Msg::Pvr));
+        }
+        Msg::Pga(msg) => {
+            pga::update(msg, &mut model.pga_model, &mut orders.proxy(Msg::Pga));
+        },
+        Msg::Test(astr) => {
+            log!(astr)
+        }
     }
 }
 
@@ -115,42 +115,42 @@ fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
 
 fn view(model: &Model) -> impl View<Msg> {
     let page_content_node: Node<Msg> = match model.page {
-	Page::EventList => {
-	    div!["This is event list page"]
-	},
-	Page::AccountState => {
-	    pas::view(&model.pas_model)
-		.map_message(Msg::Pas)
-	},
-	Page::VerifyRequest => {
-	    pvr::view(&model.pvr_model)
-		.map_message(Msg::Pvr)
-	},
-	Page::GenerateAuthorization => {
-	    pga::view(&model.pga_model)
-		.map_message(Msg::Pga)
-	}
+        Page::EventList => {
+            div!["This is event list page"]
+        },
+        Page::AccountState => {
+            pas::view(&model.pas_model)
+                .map_message(Msg::Pas)
+        },
+        Page::VerifyRequest => {
+            pvr::view(&model.pvr_model)
+                .map_message(Msg::Pvr)
+        },
+        Page::GenerateAuthorization => {
+            pga::view(&model.pga_model)
+                .map_message(Msg::Pga)
+        }
     };
 
     div![
-	div![class!["navigation"],
-	     div![class!["logo"],
-		  "Litentry Logo"
-	     ],
-	     div![class!["navigator"],
-		  div![class!["page_tab"],
-		       a!["Account State", attrs!{At::Href => "/account_state"}]
-		  ],
-		  div![class!["page_tab"],
-		       a!["Verify Request", attrs!{At::Href => "/verify_request"}]
-		  ],
-		  div![class!["page_tab"],
-		       a!["GenerateAuthorization", attrs!{At::Href => "/generate_authorization"}]
-		  ],
-	     ],
-	],
-	div![class!["page_content"],
-	     page_content_node
-	],
+        div![class!["navigation"],
+             div![class!["logo"],
+                  "Litentry Logo"
+             ],
+             div![class!["navigator"],
+                  div![class!["page_tab"],
+                       a!["Account State", attrs!{At::Href => "/account_state"}]
+                  ],
+                  div![class!["page_tab"],
+                       a!["Verify Request", attrs!{At::Href => "/verify_request"}]
+                  ],
+                  div![class!["page_tab"],
+                       a!["GenerateAuthorization", attrs!{At::Href => "/generate_authorization"}]
+                  ],
+             ],
+        ],
+        div![class!["page_content"],
+             page_content_node
+        ],
     ]
 }
